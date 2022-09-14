79aa8ff4515e67aeed9aaa6775f44f8b4f799455
Gustavo <gustavorodrigues.grm@gmail.com>
2022-09-14 14:58:57 -0300

    controllers, models and routes for "Autores"

---
+++ src/controllers/autoresController.js
@@ -0,0 +1,57 @@
+import autores from "../models/Autor.js";
+
+export default class AutorController {
+  static listarAutores = (req,res) => {
+    autores.find((err, autores)=>{
+      res.status(200).json(autores)
+    })
+  }
+
+  static listarAutorPorId = (req,res) =>  {
+    const id = req.params.id;
+
+    autores.findById(id, (err,autores) => {
+      if(err){
+        res.status(400).json({message: `${err.message} - Id do Autor não localizado!`})
+      } else {
+        res.status(200).json(autores)
+      }
+    })
+
+  }
+
+  static cadastrarAutor = (req,res) => {
+    let autor = new autores(req.body);
+    autor.save((err)=>{
+      if(err){
+        res.status(500).send({message: `${err.message} - Falha ao cadastrar Autor`})
+      } else {
+        res.status(201).send(autor.toJSON())
+      }
+    })
+  }
+
+  static atualizarAutor = (req,res) => {
+    const id = req.params.id;
+
+    autores.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err){
+        res.status(200).json({message: 'Autor atualizado com sucesso'})
+      } else {
+        res.status(500).json({message: err.message})
+      }
+    })
+  }
+
+  static excluirAutor = (req, res) => {
+    const id = req.params.id;
+
+    autores.findByIdAndDelete(id, (err) => {
+      if(!err) {
+        res.status(200).json({message: 'Autor removido com sucesso'})
+      } else {
+        res.status(500).json({message: 'Autor removido com sucesso'})
+      }
+    })
+  }
+}
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,21 +2,25 @@ import livros from "../models/Livro.js";
 
 export default class LivroController {
   static listarLivros = (req,res) => {
-    livros.find((err, livros)=>{
-      res.status(200).json(livros)
+    livros.find()
+      .populate('autor')
+      .exec((err, livros)=>{
+        res.status(200).json(livros)
     })
   }
 
   static listarLivroPorId = (req,res) =>  {
     const id = req.params.id;
 
-    livros.findById(id, (err,livros) => {
-      if(err){
-        res.status(400).json({message: `${err.message} - Id do livro não localizado!`})
-      } else {
-        res.status(200).json(livros)
-      }
-    })
+    livros.findById(id)
+      .populate('autor', 'nome')
+      .exec((err, livros) => {
+        if(err) {
+          res.status(400).send({message: `${err.message} - Id do livro não encontrado`})
+        } else {
+          res.status(200).send(livros)
+        }
+      })
 
   }
 
@@ -54,4 +58,17 @@ export default class LivroController {
       }
     })
   }
+
+  static listarLivroPorEditora = (req, res) =>{
+    const editora = req.query.editora
+
+    livros.find({'editora': editora}, {}, (err, livros) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Livros da editora ${editora} não encontrados`})
+      } else {
+        res.status(200).send(livros)
+      }
+    })
+  }
+
 }
